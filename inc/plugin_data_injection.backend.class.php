4a41e06d6a1ed29c982ff2679aff272eb6d30ed6
Walid Nouh <wnouh@teclib.com>
2007-08-22 13:24:09 +0000

    [data_injection] correction mineure

--- inc/plugin_data_injection.backend.class.php
+++ inc/plugin_data_injection.backend.class.php
@@ -115,7 +115,7 @@ class Backend {
 	function getDatasFromLineToLine($start_line,$end_line)
 	{
 		$tmp = array();
-		for ($i=$start_line;$i <= $end_line;$i++)
+		for ($i=$start_line;$i < getNumberOfLine() && $i <= $end_line;$i++)
 			$tmp[] = $this->injectionDatas->getDataAtLine($i);
 		return $tmp;	
 	}
@@ -123,6 +123,11 @@ class Backend {
 	function isFileCorrect()
 	{
 		return true;
+	}
+	
+	function getNumberOfLine()
+	{
+		return count ($this->injectionDatas->getDatas());
 	}
 }
 
