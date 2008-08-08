44bdd8d120986bb19a7f4bc5481e067f59f359a4
Walid Nouh <wnouh@teclib.com>
2008-08-08 07:11:07 +0000

    [data_injection] Forget global DB

--- inc/plugin_data_injection.config.function.php
+++ inc/plugin_data_injection.config.function.php
@@ -201,8 +201,7 @@ function plugin_data_injection_createfirstaccess($ID) {
 }
 
 function plugin_data_injection_createaccess($ID) {
-
-	GLOBAL $DB;
+	global $DB;
 	
 	$Profile = new Profile();
 	$Profile->GetfromDB($ID);
