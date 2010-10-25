fee137fac6087f579d3b536e248e8b67fb8c1e4f
yllen <nini.lasson@orange.fr>
2010-10-25 15:47:28 +0000

    coding standard

--- inc/modelcsv.class.php
+++ inc/modelcsv.class.php
@@ -31,112 +31,130 @@ class PluginDatainjectionModelcsv extends CommonDBChild {
    var $specific_fields;
 
    // From CommonDBChild
-   public $itemtype = 'PluginDatainjectionModel';
-   public $items_id = 'models_id';
+   public $itemtype  = 'PluginDatainjectionModel';
+   public $items_id  = 'models_id';
    public $dohistory = true;
 
+
    function getEmpty() {
-      $this->fields['delimiter'] = ';';
+
+      $this->fields['delimiter']         = ';';
       $this->fields['is_header_present'] = 1;
    }
 
+
    function canCreate() {
-      return plugin_datainjection_haveRight('model','w');
+      return plugin_datainjection_haveRight('model', 'w');
    }
 
+
    function canView() {
-      return plugin_datainjection_haveRight('model','r');
+      return plugin_datainjection_haveRight('model', 'r');
    }
+
 
    function init() {
    }
 
+
    //---- Getters -----//
 
-   function getDelimiter()
-   {
+   function getDelimiter() {
       return $this->fields["delimiter"];
    }
 
-   function isHeaderPresent()
-   {
+
+   function isHeaderPresent() {
       return $this->fields["is_header_present"];
    }
 
+
    //---- Save -----//
-   function setDelimiter($delimiter)
-   {
+   function setDelimiter($delimiter) {
       $this->fields["delimiter"] = $delimiter;
    }
 
-   function setHeaderPresent($present)
-   {
+
+   function setHeaderPresent($present) {
       $this->fields["is_header_present"] = $present;
    }
 
+
    /**
     * Check if filename ends with .csv
+    *
     * @param the filename
+    *
     * @return boolean true if name is correct, false is not
-    */
+   **/
    function checkFileName($filename) {
-      return ( !strstr(strtolower(substr($filename,strlen($filename)-4)), '.csv'));
+      return ( !strstr(strtolower(substr($filename, strlen($filename)-4)), '.csv'));
    }
+
 
    /**
     * Get CSV's specific ID for a model
     * If row doesn't exists, it creates it
+    *
     * @param models_id the model ID
+    *
     * @return the ID of the row in glpi_plugin_datainjection_modelcsv
-    */
+   **/
    function getFromDBByModelID($models_id) {
       global $DB;
-      $query = "SELECT `id` FROM `".$this->getTable()."` WHERE `models_id`='$models_id'";
+
+      $query = "SELECT `id`
+                FROM `".$this->getTable()."`
+                WHERE `models_id` = '$models_id'";
+
       $results = $DB->query($query);
       $id = 0;
+
       if ($DB->numrows($results) > 0) {
-         $id = $DB->result($results,0,'id');
+         $id = $DB->result($results, 0, 'id');
          $this->getFromDB($id);
-      }
-      else {
+
+      } else {
          $this->getEmpty();
          $tmp = $this->fields;
          $tmp['models_id'] = $models_id;
-         $id = $this->add($tmp);
+         $id  = $this->add($tmp);
          $this->getFromDB($id);
       }
 
       return $id;
    }
 
-   function showAdditionnalForm(PluginDatainjectionModel $model,$options=array()) {
+
+   function showAdditionnalForm(PluginDatainjectionModel $model, $options=array()) {
       global $LANG;
 
-      $id = $this->getFromDBByModelID($model->fields['id']);
-      $canedit=$this->can($id,'w');
+      $id      = $this->getFromDBByModelID($model->fields['id']);
+      $canedit = $this->can($id, 'w');
 
       echo "<tr><th colspan='4'>".$LANG['datainjection']['model'][29]."</th></tr>";
 
       echo "<tr class='tab_bg_1'>";
-
-      echo "<td>".$LANG['datainjection']['model'][9].": </td>";
+      echo "<td>".$LANG['datainjection']['model'][9]."&nbsp;: </td>";
       echo "<td>";
-      Dropdown::showYesNo('is_header_present',$this->isHeaderPresent());
+      Dropdown::showYesNo('is_header_present', $this->isHeaderPresent());
       echo "</td>";
-      echo "<td>".$LANG['datainjection']['model'][10].": </td>";
+      echo "<td>".$LANG['datainjection']['model'][10]."&nbsp;: </td>";
       echo "<td>";
       echo "<input type='text' size='1' name='delimiter' value='".$this->getDelimiter()."'";
       echo "</td>";
       echo "</tr>";
    }
 
+
    function saveFields($fields) {
-      $csv = clone $this;
-      $tmp['models_id'] = $fields['id'];
-      $tmp['delimiter'] = $fields['delimiter'];
+
+      $csv                      = clone $this;
+      $tmp['models_id']         = $fields['id'];
+      $tmp['delimiter']         = $fields['delimiter'];
       $tmp['is_header_present'] = $fields['is_header_present'];
       $csv->getFromDBByModelID($fields['id']);
-      $tmp['id'] = $csv->fields['id'];
+      $tmp['id']                = $csv->fields['id'];
       $csv->update($tmp);
    }
 
