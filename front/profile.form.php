0fef6010afed4e793daa575857c50e761b006ecf
Remi Collet <fedora@famillecollet.com>
2011-06-11 09:08:30 +0000

    fix headers, fixed #2856

--- front/profile.form.php
+++ front/profile.form.php
@@ -1,12 +1,12 @@
 <?php
 /*
- * @version $Id$
- ----------------------------------------------------------------------
+ * @version $Id: HEADER 14684 2011-06-11 06:32:40Z remi $
+ -------------------------------------------------------------------------
  GLPI - Gestionnaire Libre de Parc Informatique
- Copyright (C) 2003-2008 by the INDEPNET Development Team.
+ Copyright (C) 2003-2011 by the INDEPNET Development Team.
 
  http://indepnet.net/   http://glpi-project.org
- ----------------------------------------------------------------------
+ -------------------------------------------------------------------------
 
  LICENSE
 
@@ -23,9 +23,9 @@
  GNU General Public License for more details.
 
  You should have received a copy of the GNU General Public License
- along with GLPI; if not, write to the Free Software
- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- ------------------------------------------------------------------------
+ along with GLPI; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ --------------------------------------------------------------------------
  */
 
 // ----------------------------------------------------------------------
--- inc/model.class.php
+++ inc/model.class.php
@@ -1,9 +1,9 @@
 <?php
 /*
- * @version $Id: rules.constant.php 5351 2007-08-07 11:57:46Z walid $
+ * @version $Id: HEADER 14684 2011-06-11 06:32:40Z remi $
  -------------------------------------------------------------------------
  GLPI - Gestionnaire Libre de Parc Informatique
- Copyright (C) 2003-2008 by the INDEPNET Development Team.
+ Copyright (C) 2003-2011 by the INDEPNET Development Team.
 
  http://indepnet.net/   http://glpi-project.org
  -------------------------------------------------------------------------
@@ -23,14 +23,9 @@
  GNU General Public License for more details.
 
  You should have received a copy of the GNU General Public License
- along with GLPI; if not, write to the Free Software
- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ along with GLPI; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  --------------------------------------------------------------------------
-
-// ----------------------------------------------------------------------
-// Original Author of file: Walid Nouh
-// Purpose of file:
-// ----------------------------------------------------------------------
 */
 
 class PluginDatainjectionModel extends CommonDBTM {
