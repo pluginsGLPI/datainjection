30c9d8834471d65ba557436d495e5ed2c777d4df
Walid Nouh <wnouh@teclib.com>
2011-11-06 17:55:33 +0000

    fixed #3206 	Add LICENSE file and change file headers

--- front/export.csv.php
+++ front/export.csv.php
@@ -1,4 +1,33 @@
 <?php
+/*
+ * @version $Id: clientinjection.form.php 581 2011-06-11 09:08:30Z remi $
+ LICENSE
+
+ This file is part of the order plugin.
+
+ Datainjection plugin is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ Datainjection plugin is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with datainjection; along with Behaviors. If not, see <http://www.gnu.org/licenses/>.
+ --------------------------------------------------------------------------
+ @package   datainjection
+ @author    the datainjection plugin team
+ @copyright Copyright (c) 2010-2011 Order plugin team
+ @license   GPLv2+
+            http://www.gnu.org/licenses/gpl.txt
+ @link      https://forge.indepnet.net/projects/datainjection
+ @link      http://www.glpi-project.org/
+ @since     2009
+ ---------------------------------------------------------------------- */
+
 if (!defined('GLPI_ROOT')) {
    define('GLPI_ROOT', '../../..');
 }
--- inc/mapping.class.php
+++ inc/mapping.class.php
@@ -1,32 +1,33 @@
 <?php
 /*
  * @version $Id: HEADER 14684 2011-06-11 06:32:40Z remi $
- -------------------------------------------------------------------------
- GLPI - Gestionnaire Libre de Parc Informatique
- Copyright (C) 2003-2011 by the INDEPNET Development Team.
-
- http://indepnet.net/   http://glpi-project.org
- -------------------------------------------------------------------------
-
  LICENSE
 
- This file is part of GLPI.
+ This file is part of the order plugin.
 
- GLPI is free software; you can redistribute it and/or modify
+ Datainjection plugin is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation; either version 2 of the License, or
  (at your option) any later version.
 
- GLPI is distributed in the hope that it will be useful,
+ Datainjection plugin is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
 
  You should have received a copy of the GNU General Public License
- along with GLPI; if not, write to the Free Software Foundation, Inc.,
- 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ along with datainjection; along with Behaviors. If not, see <http://www.gnu.org/licenses/>.
  --------------------------------------------------------------------------
- */
+ @package   datainjection
+ @author    the datainjection plugin team
+ @copyright Copyright (c) 2010-2011 Order plugin team
+ @license   GPLv2+
+            http://www.gnu.org/licenses/gpl.txt
+ @link      https://forge.indepnet.net/projects/datainjection
+ @link      http://www.glpi-project.org/
+ @since     2009
+ ---------------------------------------------------------------------- */
+
 class PluginDatainjectionMapping extends CommonDBTM {
 
    //const NOT_MAPPED = -1;
