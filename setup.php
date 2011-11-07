a0b1d55b2bcb388cd89ffe90c1d31c02aa5fd5e4
Walid Nouh <wnouh@teclib.com>
2011-11-07 20:55:38 +0000

    Update version to 2.1.1

--- setup.php
+++ setup.php
@@ -103,7 +103,7 @@ function plugin_version_datainjection() {
       'minGlpiVersion' => '0.80',
       'author'         => 'Walid Nouh & Remi Collet',
       'homepage'       => 'https://forge.indepnet.net/projects/datainjection',
-      'version'        => '2.1.0'
+      'version'        => '2.1.1'
    );
 }
 
