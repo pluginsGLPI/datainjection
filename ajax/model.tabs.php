6bb5209b9deb319d380528802cf1b083127a24c7
Walid Nouh <wnouh@teclib.com>
2012-08-30 13:26:46 +0000

    TAG 2.1.8 & 2.2.3

---
+++ ajax/model.tabs.php
@@ -0,0 +1,116 @@
+<?php
+/*
+ * @version $Id$
+ LICENSE
+
+ This file is part of the order plugin.
+
+ Datainjection plugin is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ Datainjection plugin is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with datainjection. If not, see <http://www.gnu.org/licenses/>.
+ --------------------------------------------------------------------------
+ @package   datainjection
+ @author    the datainjection plugin team
+ @copyright Copyright (c) 2010-2011 Order plugin team
+ @license   GPLv2+
+            http://www.gnu.org/licenses/gpl.txt
+ @link      https://forge.indepnet.net/projects/datainjection
+ @link      http://www.glpi-project.org/
+ @since     2009
+ ---------------------------------------------------------------------- */
+
+define('GLPI_ROOT', '../../..');
+include (GLPI_ROOT . "/inc/includes.php");
+
+header("Content-Type: text/html; charset=UTF-8");
+header_nocache();
+
+if (!isset($_POST["id"])) {
+   exit();
+}
+
+if (!isset($_POST["sort"])) {
+   $_POST["sort"] = "";
+}
+
+if (!isset($_POST["order"])) {
+   $_POST["order"] = "";
+}
+
+$model = new PluginDatainjectionModel;
+if ($_POST['glpi_tab'] == 1) {
+   $model->showAdvancedForm($_POST["id"]);
+}
+
+if ($_POST["id"] >0 && $model->can($_POST["id"],'r')) {
+   switch($_POST['glpi_tab']) {
+      case -1 :
+         Plugin::displayAction($model,$_POST['glpi_tab']);
+
+         if ($model->fields['step'] > PluginDatainjectionModel::INITIAL_STEP) {
+            $options['confirm']   = 'creation';
+            $options['models_id'] = $model->fields['id'];
+            $options['add_form']  = true;
+            $options['submit']    = $LANG['datainjection']['fileStep'][13];
+            PluginDatainjectionClientInjection::showUploadFileForm($options);
+
+         } else {
+            if ($model->fields['step'] > PluginDatainjectionModel::FILE_STEP) {
+               PluginDatainjectionMapping::showFormMappings($model);
+            }
+            if ($model->fields['step'] > PluginDatainjectionModel::MAPPING_STEP) {
+               PluginDatainjectionInfo::showFormInfos($model);
+               $model->showValidationForm();
+            }
+         }
+         break;
+
+      case 3 :
+         $options['confirm']   = 'creation';
+         $options['models_id'] = $model->fields['id'];
+         $options['add_form']  = true;
+         $options['submit']    = $LANG['datainjection']['fileStep'][13];
+         PluginDatainjectionClientInjection::showUploadFileForm($options);
+         break;
+
+      case 4 :
+         PluginDatainjectionMapping::showFormMappings($model);
+         break;
+
+      case 5 :
+         if ($model->fields['step'] > PluginDatainjectionModel::MAPPING_STEP) {
+            PluginDatainjectionInfo::showFormInfos($model);
+         }
+         break;
+
+      case 6 :
+         break;
+
+      case 7:
+         if ($model->fields['step'] > PluginDatainjectionModel::MAPPING_STEP) {
+            $model->showValidationForm();
+         }
+         break;
+
+      case 12 :
+         Log::showForItem($model);
+         break;
+
+      default :
+         if (!Plugin::displayAction($model,$_POST['glpi_tab'])) {
+         }
+   }
+
+   ajaxFooter();
+}
+
+?>
---
+++ inc/consumableiteminjection.class.php
@@ -0,0 +1,99 @@
+<?php
+/*
+ * @version $Id$
+ LICENSE
+
+ This file is part of the order plugin.
+
+ Datainjection plugin is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ Datainjection plugin is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with datainjection. If not, see <http://www.gnu.org/licenses/>.
+ --------------------------------------------------------------------------
+ @package   datainjection
+ @author    the datainjection plugin team
+ @copyright Copyright (c) 2010-2011 Order plugin team
+ @license   GPLv2+
+            http://www.gnu.org/licenses/gpl.txt
+ @link      https://forge.indepnet.net/projects/datainjection
+ @link      http://www.glpi-project.org/
+ @since     2009
+ ---------------------------------------------------------------------- */
+ 
+if (!defined('GLPI_ROOT')) {
+   die("Sorry. You can't access directly to this file");
+}
+
+class PluginDatainjectionConsumableItemInjection extends ConsumableItem
+                                                 implements PluginDatainjectionInjectionInterface {
+
+   function __construct() {
+      $this->table = getTableForItemType(get_parent_class($this));
+   }
+
+
+   function isPrimaryType() {
+      return true;
+   }
+
+
+   function connectedTo() {
+      return array();
+   }
+
+
+   function getOptions($primary_type = '') {
+      global $LANG;
+
+      $tab = Search::getOptions(get_parent_class($this));
+
+      //Specific to location
+      $tab[3]['linkfield'] = 'locations_id';
+
+      $tab[8]['minvalue'] = '1';
+      $tab[8]['maxvalue'] = '100';
+      $tab[8]['step']     = 1;
+      $tab[8]['-1']       = $LANG['setup'][307];
+
+      $blacklist = PluginDatainjectionCommonInjectionLib::getBlacklistedOptions();
+      //Remove some options because some fields cannot be imported
+      $notimportable = array(80, 91, 92, 93);
+      $options['ignore_fields'] = array_merge($blacklist, $notimportable);
+      $options['displaytype']   = array("dropdown"         => array(3, 4, 23),
+                                        "user"             => array(24),
+                                        "multiline_text"   => array(16, 90),
+                                        "dropdown_integer" => array(8));
+      $options['checktype']     = array("integer" => array(8));
+      $tab = PluginDatainjectionCommonInjectionLib::addToSearchOptions($tab, $options, $this);
+
+      return $tab;
+   }
+
+
+   /**
+    * Standard method to add an object into glpi
+ 
+    *
+    * @param values fields to add into glpi
+    * @param options options used during creation
+    * @return an array of IDs of newly created objects : for example array(Computer=>1, Networkport=>10)
+    *
+   **/
+   function addOrUpdateObject($values=array(), $options=array()) {
+
+      $lib = new PluginDatainjectionCommonInjectionLib($this,$values,$options);
+      $lib->processAddOrUpdate();
+      return $lib->getInjectionResults();
+   }
+
+}
+
+?>
