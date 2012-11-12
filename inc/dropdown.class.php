66882046f87e13c6b7315ffee88183685f8cba38
Walid Nouh <wnouh@teclib.com>
2012-11-12 12:22:02 +0000

    TAG 2.1.9 for GLPI 0.80 & 2.2.4 for GLPI 0.83.3+

---
+++ inc/dropdown.class.php
@@ -0,0 +1,134 @@
+<?php
+/*
+ * @version $Id: HEADER 14684 2011-06-11 06:32:40Z remi $
+ LICENSE
+
+ This file is part of the order plugin.
+
+ Datainjection plugin is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ Datainjection plugin is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with datainjection. If not, see <http://www.gnu.org/licenses/>.
+ --------------------------------------------------------------------------
+ @package   datainjection
+ @author    the datainjection plugin team
+ @copyright Copyright (c) 2010-2011 Order plugin team
+ @license   GPLv2+
+            http://www.gnu.org/licenses/gpl.txt
+ @link      https://forge.indepnet.net/projects/datainjection
+ @link      http://www.glpi-project.org/
+ @since     2009
+ ---------------------------------------------------------------------- */
+
+class PluginDatainjectionDropdown {
+
+   static function dropdownDateFormat($format) {
+      global $LANG;
+
+      $date_format[PluginDatainjectionCommonInjectionLib::DATE_TYPE_DDMMYYYY]
+                                                            = $LANG['datainjection']['model'][22];
+      $date_format[PluginDatainjectionCommonInjectionLib::DATE_TYPE_MMDDYYYY]
+                                                            = $LANG['datainjection']['model'][23];
+      $date_format[PluginDatainjectionCommonInjectionLib::DATE_TYPE_YYYYMMDD]
+                                                            = $LANG['datainjection']['model'][24];
+
+      Dropdown::showFromArray('date_format', $date_format, array('value' => $format));
+   }
+
+
+   static function floatFormats() {
+      global $LANG;
+
+      $float_format[PluginDatainjectionCommonInjectionLib::FLOAT_TYPE_DOT]
+                                                            = $LANG['datainjection']['model'][25];
+      $float_format[PluginDatainjectionCommonInjectionLib::FLOAT_TYPE_COMMA]
+                                                            = $LANG['datainjection']['model'][26];
+      $float_format[PluginDatainjectionCommonInjectionLib::FLOAT_TYPE_DOT_AND_COM]
+                                                            = $LANG['datainjection']['model'][27];
+
+      return $float_format;
+   }
+
+
+   static function getFloatFormat($format) {
+
+      $formats = PluginDatainjectionDropdown::floatFormats();
+      if (isset($formats[$format])) {
+         return $formats[$format];
+      }
+      return "";
+   }
+
+
+   static function dropdownFloatFormat($format) {
+
+      Dropdown::showFromArray('float_format', PluginDatainjectionDropdown::floatFormats(),
+                              array('value' => $format));
+   }
+
+
+   static function dropdownFileTypes($value) {
+
+      $values['csv'] = "CSV";
+      Dropdown::showFromArray('filetype', $values, array('value' => $value));
+   }
+
+
+   static function dropdownFileEncoding() {
+      global $LANG;
+
+      $values[PluginDatainjectionBackend::ENCODING_AUTO]      = $LANG['datainjection']['fileStep'][10];
+      $values[PluginDatainjectionBackend::ENCODING_UFT8]      = $LANG['datainjection']['fileStep'][11];
+      $values[PluginDatainjectionBackend::ENCODING_ISO8859_1] = $LANG['datainjection']['fileStep'][12];
+
+      Dropdown::showFromArray('file_encoding', $values,
+                              array('value' => PluginDatainjectionBackend::ENCODING_AUTO));
+   }
+
+
+   static function portUnicityValues() {
+      global $LANG;
+
+      $values[PluginDatainjectionCommonInjectionLib::UNICITY_NETPORT_LOGICAL_NUMBER]
+                                             = $LANG["networking"][21];
+      $values[PluginDatainjectionCommonInjectionLib::UNICITY_NETPORT_NAME]
+                                             = $LANG["common"][16];
+      $values[PluginDatainjectionCommonInjectionLib::UNICITY_NETPORT_MACADDRESS]
+                                             = $LANG["device_iface"][2];
+      $values[PluginDatainjectionCommonInjectionLib::UNICITY_NETPORT_LOGICAL_NUMBER_NAME]
+                                             = $LANG["networking"][21]."+".$LANG["common"][16];
+      $values[PluginDatainjectionCommonInjectionLib::UNICITY_NETPORT_LOGICAL_NUMBER_MAC]
+                                             = $LANG["networking"][21]."+".$LANG["device_iface"][2];
+      $values[PluginDatainjectionCommonInjectionLib::UNICITY_NETPORT_LOGICAL_NUMBER_NAME_MAC]
+                                             = $LANG["networking"][21]."+".$LANG["common"][16]."+".
+                                               $LANG["device_iface"][2];
+      return $values;
+   }
+
+
+   static function dropdownPortUnicity($value) {
+
+      $values = PluginDatainjectionDropdown::portUnicityValues();
+      Dropdown::showFromArray('port_unicity', $values, array('value' => $value));
+   }
+
+
+   static function getPortUnitictyValues($value) {
+
+      $values = PluginDatainjectionDropdown::portUnicityValues();
+      if (isset($values[$value])) {
+         return $values[$value];
+      }
+      return "";
+   }
+
+}
+?>
---
+++ inc/webservice.class.php
@@ -0,0 +1,173 @@
+<?php
+/*
+ * @version $Id$
+ LICENSE
+
+ This file is part of the order plugin.
+
+ Datainjection plugin is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ Datainjection plugin is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with datainjection. If not, see <http://www.gnu.org/licenses/>.
+ --------------------------------------------------------------------------
+ @package   datainjection
+ @author    the datainjection plugin team
+ @copyright Copyright (c) 2010-2011 Order plugin team
+ @license   GPLv2+
+            http://www.gnu.org/licenses/gpl.txt
+ @link      https://forge.indepnet.net/projects/datainjection
+ @link      http://www.glpi-project.org/
+ @since     2009
+ ---------------------------------------------------------------------- */
+ 
+class PluginDatainjectionWebservice {
+
+
+   static function methodInject($params, $protocol) {
+
+      if (isset ($params['help'])) {
+         return array('uri'        => 'string,mandatory',
+                      'base64'     => 'string,optional',
+                      'additional' => 'array,optional',
+                      'models_id'  => 'integer, mandatory',
+                      'help'       => 'bool,optional');
+      }
+
+      $model = new PluginDatainjectionModel();
+
+      //-----------------------------------------------------------------
+      //-------------------------- Check parameters ---------------------
+      //-----------------------------------------------------------------
+      if (!isset ($_SESSION['glpiID'])) {
+         return PluginWebservicesMethodCommon::Error($protocol,
+                                                     WEBSERVICES_ERROR_NOTAUTHENTICATED);
+      }
+
+      if (!isset($params['uri']) && !isset($params['base64'])) {
+         return PluginWebservicesMethodCommon::Error($protocol,
+                                                     WEBSERVICES_ERROR_MISSINGPARAMETER,
+                                                     '', 'uri or base64');
+      }
+
+      if (!isset ($params['models_id'])) {
+         return PluginWebservicesMethodCommon::Error($protocol,
+                                                     WEBSERVICES_ERROR_MISSINGPARAMETER,
+                                                     'models_id');
+      } else {
+         if (!$model->getFromDB($params['models_id'])) {
+               return PluginWebservicesMethodCommon::Error($protocol,
+                                                           WEBSERVICES_ERROR_NOTFOUND,
+                                                           'Model unknown');
+               
+            } elseif (!$model->can($params['models_id'], 'r')) {
+               return PluginWebservicesMethodCommon::Error($protocol,
+                                                           WEBSERVICES_ERROR_NOTALLOWED,
+                                                           'You cannot access this model');
+            }
+
+      }
+
+      //Check entity
+      if (!isset ($params['entities_id'])) {
+         return PluginWebservicesMethodCommon::Error($protocol,
+                                                     WEBSERVICES_ERROR_MISSINGPARAMETER,
+                                                     'entities_id');
+      } else {
+         $entities_id = $params['entities_id'];
+         if ($entities_id > 0 ) {
+            $entity = new Entity();
+            if (!$entity->getFromDB($entities_id)) {
+               return PluginWebservicesMethodCommon::Error($protocol,
+                                                           WEBSERVICES_ERROR_NOTFOUND,
+                                                           'Entity unknown');
+               
+            } elseif (!haveAccessToEntity($entities_id)) {
+               return PluginWebservicesMethodCommon::Error($protocol,
+                                                           WEBSERVICES_ERROR_NOTALLOWED,
+                                                           'You cannot access this entity');
+            }
+         }
+      }
+
+      //Mandatory fields
+      $additional_infos = array();
+      if (isset($params['additional']) && is_array($params['additional'])) {
+         $additional_infos = $params['additional'];
+      }
+      
+      
+      //Upload CSV file
+      $document_name = basename($params['uri']);
+      $filename      = tempnam(PLUGIN_DATAINJECTION_UPLOAD_DIR, 'PWS');
+      $response      = PluginWebservicesMethodCommon::uploadDocument($params, $protocol, $filename,
+                                                                     $document_name);
+
+      if (PluginWebservicesMethodCommon::isError($protocol, $response)) {
+         return $response;
+      }
+
+      //Uploade successful : now perform import !
+      $options = array('file_encoding'     => PluginDatainjectionBackend::ENCODING_AUTO, //Detect automatically file encoding
+                       'webservice'        => true, //Use webservice CSV file import
+                       'original_filename' => $params['uri'], //URI to the CSV file
+                       'unique_filename'   => $filename, //Unique filename
+                       'mode'              => PluginDatainjectionModel::PROCESS,
+                       'delete_file'       => false, //Do not delete file once imported
+                       'protocol'          => $protocol); //The Webservice protocol used
+
+      $results  = array();
+      $response = $model->processUploadedFile($options);
+      if (!PluginWebservicesMethodCommon::isError($protocol, $response)) {
+         $engine  = new PluginDatainjectionEngine($model, $additional_infos, $params['entities_id']);
+         //Remove first line if header is present
+         $first = true;
+         foreach ($model->injectionData->getDatas() as $id => $data) {
+            if ($first && $model->getSpecificModel()->isHeaderPresent()) {
+               $first = false;
+            } else {
+               $results[] = $engine->injectLine($data[0], $id);
+            }
+         }
+         $model->cleanData();
+         return $results;
+      } else {
+          return $response;
+      }
+   }
+
+
+   static function methodGetModel($params,$protocol) {
+      $params['itemtype'] = 'PluginDatainjectionModel';
+      return PluginWebservicesMethodInventaire::methodGetObject($params, $protocol);
+   }
+
+
+  static function methodListModels($params, $protocol) {
+      $params['itemtype'] = 'PluginDatainjectionModel';
+      return PluginWebservicesMethodInventaire::methodListObjects($params, $protocol);
+   }
+
+
+   static function methodListItemtypes($params, $protocol) {
+
+      if (isset ($params['help'])) {
+         return array('help' => 'bool,optional');
+      }
+
+      if (!isset ($_SESSION['glpiID'])) {
+         return self::Error($protocol, WEBSERVICES_ERROR_NOTAUTHENTICATED);
+      }
+
+      return PluginDatainjectionInjectionType::getItemtypes();
+   }
+
+}
+?>
