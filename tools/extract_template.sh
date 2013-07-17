e8ca1cee41cae9a90fe737c3bb11aaa7976c7ea1
Xavier CAILLAUD <tsmr@users.noreply.github.com>
2013-07-17 15:28:52 +0000

    Update locales

--- tools/extract_template.sh
+++ tools/extract_template.sh
@@ -9,7 +9,7 @@ copyright='INDEPNET Development Team'
 
 # Only strings with domain specified are extracted (use Xt args of keyword param to set number of args needed)
 
-xgettext *.php */*.php --copyright-holder='Datainjection Development Team' --package-name='GLPI - Datainjection plugin' --package-version='2.2.0' -o locales/glpi.pot -L PHP --add-comments=TRANS --from-code=UTF-8 --force-po  \
+xgettext *.php */*.php --copyright-holder='Datainjection Development Team' --package-name='GLPI - Datainjection plugin' --package-version='2.3.0' -o locales/glpi.pot -L PHP --add-comments=TRANS --from-code=UTF-8 --force-po  \
 	--keyword=_n:1,2,4t --keyword=__s:1,2t --keyword=__:1,2t --keyword=_e:1,2t --keyword=_x:1c,2,3t \
 	--keyword=_ex:1c,2,3t --keyword=_nx:1c,2,3,5t --keyword=_sx:1c,2,3t
 
