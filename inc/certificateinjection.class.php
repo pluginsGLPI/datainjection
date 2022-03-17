59bf986151afeac7ed719667be419af69479993c
fabrepe <contact@fabrepe.com>
2022-03-17 12:47:08 +0100

    Add certificate injection capability; closes #151

---
+++ inc/certificateinjection.class.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ * -------------------------------------------------------------------------
+ * DataInjection plugin for GLPI
+ * -------------------------------------------------------------------------
+ *
+ * LICENSE
+ *
+ * This file is part of DataInjection.
+ *
+ * DataInjection is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * DataInjection is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DataInjection. If not, see <http://www.gnu.org/licenses/>.
+ * -------------------------------------------------------------------------
+ * @copyright Copyright (C) 2007-2022 by DataInjection plugin team.
+ * @license   GPLv2 https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://github.com/pluginsGLPI/datainjection
+ * -------------------------------------------------------------------------
+ */
+
+if (!defined('GLPI_ROOT')) {
+    die("Sorry. You can't access directly to this file");
+}
+
+class PluginDatainjectionCertificateInjection extends Certificate
+                                            implements PluginDatainjectionInjectionInterface
+{
+
+   static function getTable($classname = null) {
+      $parenttype = get_parent_class();
+      return $parenttype::getTable();
+   }
+
+   function isPrimaryType() {
+      return true;
+   }
+
+   function connectedTo() {
+      return [];
+   }
+
+   /**
+    * @see plugins/datainjection/inc/PluginDatainjectionInjectionInterface::getOptions()
+   **/
+   function getOptions($primary_type = '') {
+
+      $tab           = Search::getOptions(get_parent_class($this));
+
+      //Remove some options because some fields cannot be imported
+      $blacklist     = PluginDatainjectionCommonInjectionLib::getBlacklistedOptions(get_parent_class($this));
+      $notimportable = [2, 14, 19, 61, 72, 121]; 
+ 
+      $options['ignore_fields'] = array_merge($blacklist, $notimportable);
+      $options['displaytype']   = [
+               "multiline_text" => [11, 12, 13, 15],
+               "dropdown"       => [7, 23, 24, 31, 49, 71, 80], 
+               "user"           => [24, 70], 
+               "bool"           => [9, 86]
+      ];
+
+      return PluginDatainjectionCommonInjectionLib::addToSearchOptions($tab, $options, $this);
+   }
+
+   /**
+    * @see plugins/datainjection/inc/PluginDatainjectionInjectionInterface::addOrUpdateObject()
+   **/
+   function addOrUpdateObject($values = [], $options = []) {
+      $lib = new PluginDatainjectionCommonInjectionLib($this, $values, $options);
+      $lib->processAddOrUpdate();
+      return $lib->getInjectionResults();
+   }
+}
