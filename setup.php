917a864861111995b22f326970312615e031ad8b
Cédric Anne <cedric.anne@gmail.com>
2022-04-21 15:17:01 +0200

    2.11.1

--- setup.php
+++ setup.php
@@ -28,7 +28,7 @@
  * -------------------------------------------------------------------------
  */
 
-define ('PLUGIN_DATAINJECTION_VERSION', '2.11.0');
+define ('PLUGIN_DATAINJECTION_VERSION', '2.11.1');
 
 // Minimal GLPI version, inclusive
 define("PLUGIN_DATAINJECTION_MIN_GLPI", "10.0.0");
