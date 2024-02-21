2aba5e2d600abf2be9cfad2c67be9fd769ba0ac6
Stanislas <skita@teclib.com>
2024-02-21 09:41:20 +0100

    Allow lockedfield update

--- inc/commoninjectionlib.class.php
+++ inc/commoninjectionlib.class.php
@@ -1552,6 +1552,11 @@ class PluginDatainjectionCommonInjectionLib
                self::logAddOrUpdate($item, $add);
             }
          } else {
+            // allow to update locked fields from datainjection
+            // for dynamic item if needed
+            if ($item->maybeDynamic()) {
+               $toinject['is_dynamic'] = 0;
+            }
             if ($item->update($toinject)) {
                $newID = $toinject['id'];
                self::logAddOrUpdate($item, $add);
