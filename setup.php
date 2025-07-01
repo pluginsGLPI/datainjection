a8079e32a3937171ecfa0c1a046f54809745c522
Stanislas <skita@teclib.com>
2025-07-01 09:57:51 +0200

    bump setup

--- setup.php
+++ setup.php
@@ -28,7 +28,7 @@
  * -------------------------------------------------------------------------
  */
 
-define('PLUGIN_DATAINJECTION_VERSION', '2.14.1');
+define('PLUGIN_DATAINJECTION_VERSION', '2.15.0-beta1');
 
 // Minimal GLPI version, inclusive
 define("PLUGIN_DATAINJECTION_MIN_GLPI", "11.0.0");
