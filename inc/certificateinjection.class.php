e292cbc5411f30de6edf49055413bac5caa516b4
Lainow <texamaxsam61@gmail.com>
2025-09-03 09:34:46 +0200

    Fix indentation

--- inc/certificateinjection.class.php
+++ inc/certificateinjection.class.php
@@ -78,14 +78,14 @@ class PluginDatainjectionCertificateInjection extends Certificate implements Plu
    **/
     public function addOrUpdateObject($values = [], $options = [])
     {
-    if (isset($values['Certificate']['date_expiration'])) {
-        // Convert date to yyyy-mm-dd format
-        $date = $values['Certificate']['date_expiration'];
-        $dateTime = new DateTime($date);
-        $values['Certificate']['date_expiration'] = $dateTime->format('Y-m-d');
+        if (isset($values['Certificate']['date_expiration'])) {
+            // Convert date to yyyy-mm-dd format
+            $date = $values['Certificate']['date_expiration'];
+            $dateTime = new DateTime($date);
+            $values['Certificate']['date_expiration'] = $dateTime->format('Y-m-d');
+        }
+        $lib = new PluginDatainjectionCommonInjectionLib($this, $values, $options);
+        $lib->processAddOrUpdate();
+        return $lib->getInjectionResults();
     }
-    $lib = new PluginDatainjectionCommonInjectionLib($this, $values, $options);
-    $lib->processAddOrUpdate();
-    return $lib->getInjectionResults();
-}
 }
