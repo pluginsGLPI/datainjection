7a22a16848408c7749c57d4198f0d5d38b29af8d
Stanislas <skita@teclib.com>
2025-11-07 08:17:25 +0100

    Fix(Model): fix clean function (#543)
    * Fix(Model): fix clean function
    
    * refactor

--- inc/model.class.php
+++ inc/model.class.php
@@ -837,10 +837,7 @@ class PluginDatainjectionModel extends CommonDBTM
         $model = new self();
 
         if ((count($crit) > 0)) {
-            $crit['FIELDS'] = 'id';
-            foreach ($DB->doQuery($model->getTable()) as $row) {
-                $model->delete($row);
-            }
+            $model->deleteByCriteria($crit);
         }
     }
 
