19958521f82f925ebe61811eccd7d8aa87100be1
Samuel Launay <107540223+Lainow@users.noreply.github.com>
2025-12-16 15:29:13 +0100

    Fix location completename update (#565)
    * Fix location completename update
    
    * Update changelog
    
    * Fix header
    
    * Fix phpcs and rector

--- inc/locationinjection.class.php
+++ inc/locationinjection.class.php
@@ -30,6 +30,36 @@
 
 
 
+/**
+ * -------------------------------------------------------------------------
+ * DataInjection plugin for GLPI
+ * -------------------------------------------------------------------------
+ *
+ * LICENSE
+ *
+ * This file is part of DataInjection.
+ *
+ * DataInjection is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * DataInjection is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DataInjection. If not, see <http://www.gnu.org/licenses/>.
+ * -------------------------------------------------------------------------
+ * @copyright Copyright (C) 2007-2023 by DataInjection plugin team.
+ * @license   GPLv2 https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://github.com/pluginsGLPI/datainjection
+ * -------------------------------------------------------------------------
+ */
+
+
+
 class PluginDatainjectionLocationInjection extends Location implements PluginDatainjectionInjectionInterface
 {
     public static function getTable($classname = null)
@@ -83,9 +113,20 @@ class PluginDatainjectionLocationInjection extends Location implements PluginDat
    **/
     public function addOrUpdateObject($values = [], $options = [])
     {
-
+        $values = $this->fixLocationTreeStructure($values);
         $lib = new PluginDatainjectionCommonInjectionLib($this, $values, $options);
         $lib->processAddOrUpdate();
         return $lib->getInjectionResults();
+    }
+
+    public function fixLocationTreeStructure($values)
+    {
+        if (isset($values['Location']['completename']) && !isset($values['Location']['name']) && !str_contains($values['Location']['completename'], '>')) {
+            $values['Location']['name'] = trim($values['Location']['completename']);
+            $values['Location']['locations_id'] = '0';
+            $values['Location']['ancestors_cache'] = '[]';
+        }
+
+        return $values;
     }
 }
