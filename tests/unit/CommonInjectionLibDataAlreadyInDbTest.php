5d7202d277d820be257187e2a29c32799d3d3105
Herafia <llenik@teclib.com>
2026-05-28 14:44:54 +0200

    header check

--- tests/unit/CommonInjectionLibDataAlreadyInDbTest.php
+++ tests/unit/CommonInjectionLibDataAlreadyInDbTest.php
@@ -2,28 +2,30 @@
 
 /**
  * -------------------------------------------------------------------------
- * Escalade plugin for GLPI
+ * DataInjection plugin for GLPI
  * -------------------------------------------------------------------------
  *
  * LICENSE
  *
- * This file is part of Escalade.
+ * This file is part of DataInjection.
  *
- * Escalade is free software; you can redistribute it and/or modify
+ * DataInjection is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * Escalade is distributed in the hope that it will be useful,
+ * DataInjection is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Escalade. If not, see <http://www.gnu.org/licenses/>.
+ * along with DataInjection. If not, see <http://www.gnu.org/licenses/>.
  * -------------------------------------------------------------------------
+ * @copyright Copyright (C) 2007-2023 by DataInjection plugin team.
  * @copyright Copyright (C) 2015-2023 by Escalade plugin team.
  * @license   GPLv2 https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://github.com/pluginsGLPI/datainjection
  * @link      https://github.com/pluginsGLPI/escalade
  * -------------------------------------------------------------------------
  */
