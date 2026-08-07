9c12f24278d36fd361a4c7e936dbd49953daf751
Rom1-B <8530352+Rom1-B@users.noreply.github.com>
2026-08-07 10:28:31 +0200

    2.15.10

--- setup.php
+++ setup.php
@@ -31,7 +31,7 @@
 use function Safe\define;
 use function Safe\mkdir;
 
-define('PLUGIN_DATAINJECTION_VERSION', '2.15.9');
+define('PLUGIN_DATAINJECTION_VERSION', '2.15.10');
 
 // Minimal GLPI version, inclusive
 define("PLUGIN_DATAINJECTION_MIN_GLPI", "11.0.5");
